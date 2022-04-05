3971d0e8f6ff8ca9286eb11c20ac7c41fbe4f217
20183220 <dudgnl6032@naver.com>
2022-04-05 18:56:52 +0900

    none

---
+++ src/auaphonics.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        hi
+    </div>
+</template>
+
+  <<script>
+  import { defineComponent } from '@vue/composition-api'
+  
+  export default defineComponent({
+      setup() {
+          
+      },
+  })
+  </script>
+
+  <style>
+  </style>
+  
