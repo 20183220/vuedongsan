acd84160d31b33a8978d807f8819db0705c68f6f
20183220 <dudgnl6032@naver.com>
2022-04-28 18:00:19 +0900

    okok

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,12 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Oneroom from "./components/one-room-good.vue"
 
 export default{
   name:'App',
 
   components:{
-    HelloWorld,
     Oneroom : Oneroom
   }
 }
